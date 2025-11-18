044fd1055c553dd69159d37b5028b07873a0eeb6
Byte-Master63 <thokozanetek@gmail.com>
2025-11-18 22:11:13 +0200

    Button import removed

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,7 +1,6 @@
 import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthContext';
-import Button from '../Button/Button';
 import './Header.css';
 
 function Header() {
