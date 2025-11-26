a5f93f5567fae8216cca7ecd761b28ddca47d423
Byte-Master63 <thokozanetek@gmail.com>
2025-11-26 20:51:01 +0200

    Updated index.js for theme context

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -53,7 +53,6 @@ function Header() {
                     </button>
                   </div>
                 )}
-            )}
           </div>
         </nav>
       </div>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
 import { FinanceProvider } from './contexts/FinanceContext';
+import { ThemeProvider } from './contexts/ThemeContext';
 import ErrorBoundary from './components/ErrorBoundary';
 import './index.css';
 
@@ -13,13 +14,15 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <ErrorBoundary>
-      <Router>
-        <AuthProvider>
-          <FinanceProvider>
-            <App />
-          </FinanceProvider>
-        </AuthProvider>
-      </Router>
+      <ThemeProvider>
+        <Router>
+          <AuthProvider>
+            <FinanceProvider>
+              <App />
+            </FinanceProvider>
+          </AuthProvider>
+        </Router>
+      </ThemeProvider>
     </ErrorBoundary>
   </React.StrictMode>
 );
